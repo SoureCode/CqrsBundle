8e33e07a3f7085797c34d4a704bd45a8ee87dd0b
chapterjason <jason@sourecode.dev>
2021-10-05 22:24:57 +0200

    fix(cqrs): Change order of parameters in CommandBus constructor

--- config/services.php
+++ config/services.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the SoureCode package.
+ *
+ * (c) Jason Schilling <jason@sourecode.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use SoureCode\Component\Cqrs\CommandBus;
 use SoureCode\Component\Cqrs\CommandBusInterface;
@@ -7,6 +15,7 @@ use SoureCode\Component\Cqrs\EventBusInterface;
 use SoureCode\Component\Cqrs\QueryBus;
 use SoureCode\Component\Cqrs\QueryBusInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container) {
     $services = $container->services();
@@ -17,7 +26,10 @@ return static function (ContainerConfigurator $container) {
         ->alias(EventBusInterface::class, 'soure_code.cqrs.event_bus')
         ->public();
 
-    $services->set('soure_code.cqrs.command_bus', CommandBus::class);
+    $services->set('soure_code.cqrs.command_bus', CommandBus::class)
+        ->args([
+            service('soure_code.cqrs.event_bus'),
+        ]);
 
     $services
         ->alias(CommandBusInterface::class, 'soure_code.cqrs.command_bus')
